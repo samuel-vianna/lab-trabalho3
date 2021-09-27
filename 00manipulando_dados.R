ef983c26749d1102e7670e997f23ed0b79090436
Samuel <samuel-vianna@hotmail.com>
2021-09-27 18:23:08 -0300

    first commit

--- 00manipulando_dados.R
+++ 00manipulando_dados.R
@@ -1,46 +1,19 @@
 library(readxl)
 library(dplyr)
 library(stringr)
-library(geosphere)
 
-##### m�dicos #####
+##### reading the data #####
+data <- read_xlsx('./data/Dados abelhas.xlsx', sheet = 1)
 
-medicos <- read_xlsx('./data/dados_raw.xlsx', sheet = 2)
-
-str_split(medicos$Local, ' ') %>% 
-  sapply(function(x) {
-    return(x[2])
-  }) -> medicos$Local
-
-head(medicos)
-
-write.table(medicos, './data/medicos.csv', sep=',', row.names = F)
-
-##### pacientes #####
-
-pacientes <- read_xlsx('./data/dados_raw.xlsx')
-head(pacientes)
-
-# dist�ncia
-dist <- numeric(length(pacientes$Medico))
-
-for(i in seq_along(pacientes$Medico)){
-  ind <- grep(pacientes[i,3], LETTERS[1:7])
-  dist[i] <- distHaversine(pacientes[i,c(2,1)], medicos[ind, c(3,2)])
-}
-
-pacientes$dist <- round(dist / 1000, 3)
-
-
-# categorizando idade
-
-pacientes$categoria <- case_when(
-  pacientes$Idade <= 20 ~ 'menor que 20',
-  pacientes$Idade <= 40 ~ '20 a 40',
-  pacientes$Idade <= 60 ~ '40 a 60',
-  TRUE ~ 'acima de 60'
+# renaming columns
+names(data) <- c(
+  'meses', 'colonias', 'comp_canudo', 'diam_canudo',
+  'n_potes_mel', 'n_discos', 'tam_discos', 'peso', 'est_pop'
 )
 
+# converting months
+data$meses <- month.abb[data$meses]
 
-write.table(pacientes, './data/pacientes.csv', sep=',', row.names = F)
 
+###############################
+write.table(data, './data/data.csv', sep=',')
